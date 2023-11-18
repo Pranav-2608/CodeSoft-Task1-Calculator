63ee5028c0098b67343967ca8a1ebb1179f89898
Pranav-2608 <112697577+Pranav-2608@users.noreply.github.com>
2023-11-18 21:42:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// Todo: Make M+ M- and MC functional
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.getElementById('result').textContent = string;
+    }
+    else if(e.target.innerHTML == 'AC'){
+      string = ""
+      document.getElementById('result').textContent= string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.getElementById('result').textContent = string;
+      }
+  })
+})
